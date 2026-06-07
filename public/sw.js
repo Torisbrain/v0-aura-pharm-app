f3b1cf09542ce27b128c3ec9e1436bd0fe4166be
Victoria Robin <victoriarobintoris32@gmail.com>
2026-06-07 20:06:40 +0000

    feat: Full PWA setup - manifest, icons, service worker, offline support

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,37 @@
-const CACHE_NAME = 'aurabridge-v1';
-const STATIC_ASSETS = ['/', '/manifest.json'];
+const CACHE_NAME = 'aurabridge-v1'
+const STATIC_ASSETS = [
+  '/',
+  '/pharmverify',
+  '/interactions',
+  '/consult',
+  '/manifest.json'
+]
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
-  );
-  self.skipWaiting();
-});
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  )
+  self.skipWaiting()
+})
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
-  );
-  self.clients.claim();
-});
+  )
+  self.clients.claim()
+})
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+    fetch(event.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone))
+        return res
+      })
+      .catch(() => caches.match(event.request))
+  )
+})
